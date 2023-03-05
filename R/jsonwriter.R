369f47aaf6e1b62922c1b7615eeba0849df770ee
Gabor Nyeki <gabor.nyeki@alumni.duke.edu>
2023-03-05 11:31:02 -0500

    jsonwriter.R: add argument `extra_fields`, move `verbose` to method

--- R/jsonwriter.R
+++ R/jsonwriter.R
@@ -14,20 +14,31 @@ call_to_string <- function(call) {
 #' @param model Object that contains the regression results.  Currently
 #'              only `fixest` is supported.
 #' @param output_filename Filename to write the JSON output to.
-#' @param verbose Flag to indicate whether to write console messages.
 #' @param ... Additional arguments passed on to the method.
 #'
 #' @return Nothing.
 #' @export
-write_json <- function(model, output_filename, verbose = FALSE, ...) {
+write_json <- function(model, output_filename, ...) {
     UseMethod("write_json")
 }
 
 #' @export
 write_json.default <- function(model, output_filename, ...) NULL
 
+#' Write a `fixest` object to a JSON file.
+#'
+#' @param model A `fixest` object that contains the regression results.
+#' @param output_filename Filename to write the JSON output to.
+#' @param verbose Flag to indicate whether to write console messages.
+#' @param extra_fields A `list` object with extra information to add to
+#'                     the output.
+#' @param ... Additional arguments.  Ignored.
 #' @export
-write_json.fixest <- function(model, output_filename, verbose, ...) {
+write_json.fixest <- function(model,
+                              output_filename,
+                              verbose = FALSE,
+                              extra_fields = NULL,
+                              ...) {
     output_object <- list(
         call      = call_to_string(model$call),
         r_squared = 1 - model$ssr / model$ssr_null,
@@ -54,6 +65,15 @@ write_json.fixest <- function(model, output_filename, verbose, ...) {
         ),
         nobs      = model$nobs
     )
+
+    if (!is.null(extra_fields)) {
+        if (!is.list(extra_fields)) {
+            stop(paste("If 'extra_fields' is specified,",
+                       "then it must be a list."))
+        }
+
+        output_object <- c(output_object, extra_fields)
+    }
 
     if (verbose) {
         cat(sprintf("Writing to '%s'.\n", output_filename))
