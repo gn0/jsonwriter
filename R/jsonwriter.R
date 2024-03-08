47ca16d6d768f9056947ab77770a95637decc688
Gabor Nyeki <gabor.nyeki@alumni.duke.edu>
2024-03-08 12:17:00 -0500

    write_json.fixest: add support for non-linear models, e.g., `fepois`

--- R/jsonwriter.R
+++ R/jsonwriter.R
@@ -20,9 +20,13 @@ call_to_string <- function(call) {
 #'                     the output.  (Optional.)
 #' @param ... Additional arguments passed on to the method.
 #'
-#' @return Nothing.
+#' @return \value{No return value, called for side effects.}
 #' @examples
-#' \dontrun{library(fixest)
+#' \dontrun{# The following example fits a regression on the built-in
+#' # 'quakes' data set and writes the results to a file called
+#' # "jsonwriter_example.json" in the current working directory.
+#' #
+#' library(fixest)
 #' data(quakes)
 #'
 #' write_json(
@@ -56,39 +60,65 @@ write_json.default <- function(model,
 #'                (Optional.)
 #' @param extra_fields A 'list' object with extra information to add to
 #'                     the output.  (Optional.)
+#' @param vcov Standard error specification to pass to
+#'             'fixest::coeftable'.  (Optional.)
 #' @param ... Additional arguments.  Ignored.
+#' @return \value{No return value, called for side effects.}
 #' @export
 write_json.fixest <- function(model,
                               output_filename,
                               verbose = FALSE,
                               extra_fields = NULL,
+                              vcov = NULL,
                               ...) {
+    model_coeftable <- fixest::coeftable(model, vcov = vcov)
+
     output_object <- list(
         call      = call_to_string(model$call),
-        r_squared = 1 - model$ssr / model$ssr_null,
         coef      = sapply(
             names(model$coefficients),
             function(coef_name) {
-                p_value <- model$coeftable[coef_name, "Pr(>|t|)"]
-
-                list(
-                    est = model$coefficients[coef_name],
-                    se = model$se[coef_name],
-                    t = model$coeftable[coef_name, "t value"],
-                    p = p_value,
-                    stars = dplyr::case_when(
-                        p_value < .01 ~ "***",
-                        p_value < .05 ~ "**",
-                        p_value < .10 ~ "*",
-                        TRUE          ~ ""
-                    )
+                coef <- list(
+                    est = model_coeftable[coef_name, "Estimate"],
+                    se = model_coeftable[coef_name, "Std. Error"]
                 )
+
+                if ("Pr(>|t|)" %in% colnames(model_coeftable)) {
+                    coef$t <- model_coeftable[coef_name, "t value"]
+                    coef$p <- model_coeftable[coef_name, "Pr(>|t|)"]
+                } else if ("Pr(>|z|)" %in% colnames(model_coeftable)) {
+                    coef$z <- model_coeftable[coef_name, "z value"]
+                    coef$p <- model_coeftable[coef_name, "Pr(>|z|)"]
+                } else {
+                    warning(sprintf(
+                        "No t-value or z-value and no p-value found for '%s'.",
+                        output_filename
+                    ))
+                }
+
+                if (!is.null(coef$p)) {
+                    coef$stars <- dplyr::case_when(
+                                             coef$p < .01 ~ "***",
+                                             coef$p < .05 ~ "**",
+                                             coef$p < .10 ~ "*",
+                                             TRUE         ~ ""
+                                         )
+                }
+
+                coef
             },
             USE.NAMES = TRUE,
             simplify = FALSE
         ),
+        se_type   = attributes(model_coeftable)$type,
         nobs      = model$nobs
     )
+
+    if (!is.null(model$ssr)) {
+        output_object$r_squared <- 1 - model$ssr / model$ssr_null
+    } else if (!is.null(model$pseudo_r2)) {
+        output_object$pseudo_r_squared <- model$pseudo_r2
+    }
 
     if (!is.null(extra_fields)) {
         if (!is.list(extra_fields)) {
